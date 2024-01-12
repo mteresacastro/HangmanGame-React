081747b988efaa21a969d1b12412d33763f74956
mteresacastro <m.teresacastrogonzalez@gmail.com>
2024-01-12 15:52:01 +0100

    mobile version

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -5,10 +5,10 @@ const Instructions = () => {
 
     <section className="instructions">
       <p>
-        Existen dos modos de juego, individual, donde la palabra viene de una API, o multijugador, donde un jugador piensa en una palabra, y la escribe en la seccion de más opciones.
+        Existen dos modos de juego, individual, donde la palabra viene de una API, o multijugador, donde un jugador piensa en una palabra, y la escribe en la sección de más opciones.
       </p>
       <p>
-        La metodología de juego es la siguiente, en la seccion principal apareceran los huecos de las letras de la palabra elegida o la generada por la API. El contrincante tendrá que ir escribiendo letras sin números ni signos de puntuación, intentando adivinar la palabra de la solución.
+        La metodología de juego es la siguiente, en la sección principal aparecerán los huecos de las letras de la palabra elegida o la generada por la API. El contrincante tendrá que ir escribiendo letras sin números ni signos de puntuación, intentando adivinar la palabra de la solución.
       </p>
       <p>
         Si la letra propuesta está en la palabra a adivinar, aparecerá escrita en su hueco coresponciente, si no está, se dibujará una línea en el dibujo del ahorcado, y se añadirá a letras falladas. El jugador tendrá que adivinar la palabra antes de que el muñeco esté totalmente dibujado, de lo contrario, perderá la partida.
